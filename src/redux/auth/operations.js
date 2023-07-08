45b67f0e48a996f71fc86e0bac9479ae672b1fe1
Jacek Pasierb <jack_byk@o2.pl>
2023-07-09 01:59:13 +0200

    save

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -1,3 +1,4 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 axios.defaults.baseURL = "https://goit-task-manager.herokuapp.com/";
@@ -9,3 +10,33 @@ const setAuthHeader = (token) => {
 const clearAuthHeader = () => {
     axios.defaults.headers.common.Authorization = "";
 }
+
+export const register = createAsyncThunk(
+    "auth/register",
+    async (credentials, thunkAPI) => {
+        try {
+            const response = await axios.post(`/users/signup`, credentials);
+            return response.data;
+        } catch (e) {
+            return thunkAPI.rejectWithValue(e.message)
+        }
+    }
+)
+
+export const logIn = createAsyncThunk(
+    "auth/login",
+    async (credentials, thunkAPI) => {
+        try {
+            const response = await axios.post('/users/login', credentials);
+            
+            setAuthHeader(response.data.token);
+            return response.data;
+        } catch (e) {
+            return thunkAPI.rejectWithValue(e.message)
+        }
+    }
+);
+
+// export const logOut;
+
+// export const refreshUser;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,26 +1,26 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { tasksReducer } from "./taskSlice";
 import { filtersReducer } from "./filtersSlice";
-import { FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE, persistReducer } from "redux-persist";
+import { persistReducer, persistStore } from "redux-persist";
 import storage from "redux-persist/lib/storage"
 import { authReducer } from "./auth/silce";
 
+
 const authPersistConfig  = {
-  key: "auth",
+  key: `auth`,
   storage,
-  whiteList: ["token"],
+  whitelist: ["token"]
 }
-const middleware = {
-  serializableCheck: {
-    ignoredActions:[FLUSH,REHYDRATE,PAUSE,PERSIST,PURGE,REGISTER ],
-  }
-}
+
 
 export const store = configureStore({
   reducer: {
-    auth: persistReducer({authPersistConfig},authReducer),
+    auth: persistReducer(authPersistConfig,authReducer),
     tasks: tasksReducer,
     filters: filtersReducer,
   },
-  middleware:(getDefaultMiddleware)=>getDefaultMiddleware().concat(middleware)
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+    serializableCheck:false
+  })
 });
+export const persistor = persistStore(store);
